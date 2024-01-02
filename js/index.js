87e0fc318ea1c6c4a8774e4709c1473855613e3f
Mohammed Sabr <mohammed@Air-de-Mohammed.lan>
2024-01-02 19:50:04 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://noes6vmnzg3rvbxwyeafwo6ozm0liinr.lambda-url.eu-west-3.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `  ${data} Views!`;
+    console.log(data);
+}
+
+updateCounter();
