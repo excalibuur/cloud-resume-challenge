1d18f664580cfacd2aac16708725339d1b42d920
excalibuur <mohammedsabrms@gmail.com>
2024-01-03 19:47:39 +0100

    commit améliorations sur le CV

--- website/js/index.js
+++ website/js/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://noes6vmnzg3rvbxwyeafwo6ozm0liinr.lambda-url.eu-west-3.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `  ${data} Views!`;
+    counter.innerHTML = `  ${data} Vues !`;
     console.log(data);
 }
 
